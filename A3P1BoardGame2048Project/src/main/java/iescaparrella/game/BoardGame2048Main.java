040f65aec516ef329ab7f4bcc8a789f9d8d90283
JuliaCarullaDAM <jcarulla2@inscaparrella.cat>
2023-12-19 17:00:00 +0100

    Esborrar espais extra
    Esborrar espais extra després de l'últim commit

--- A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
+++ A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
@@ -45,13 +45,12 @@ public class BoardGame2048Main {
 
     static String mostrarMatriu(int matriu[][]) {
         String str = "";
-        
+
         for (int row = 0; row < matriu.length; row++) {
             str += "| ";
             for (int col = 0; col < matriu[row].length; col++) {
                 if (matriu[row][col] == 0) str += "\t| ";
                 else str += matriu[row][col] + " |";
-
             }
             str += "\n";
         }
@@ -80,59 +79,50 @@ public class BoardGame2048Main {
         matriu[rnd1][rnd2] = SPAWN;
     }
 
-}
-    static void gameMovement (int [][] matriu, char wasd){
-        if (wasd=='w'){
+    static void gameMovement(int[][] matriu, char wasd) {
+        if (wasd == 'w') {
 
             //funcio aux moviment cap a dalt
-        }else if (wasd=='s'){
+        } else if (wasd == 's') {
 
             //funcio aux moviment cap a baix
-        } else if (wasd == 'a'){
+        } else if (wasd == 'a') {
 
             //funcio aux moviment cap a  esquerra
-        } else if (wasd=='d'){
+        } else if (wasd == 'd') {
 
             //funcio aux moviment cap a dreta
         }
         // funcio que generi el 2 a la matriu si hi ha com a mínim un 0 a la matriu
-
     }
 
-    static void generate2 (int [][] matriu){
-        Random rnd=new Random();
-        boolean chivato=false;
-        for (int i = 0; i < matriu.length; i++){
-            for (int x = 0; x < matriu[0].length; x++){
-                if (matriu[i][x] == 0){
-                    chivato=true;
+    static void generate2(int[][] matriu) {
+        Random rnd = new Random();
+        boolean chivato = false;
+        for (int i = 0; i < matriu.length; i++) {
+            for (int x = 0; x < matriu[0].length; x++) {
+                if (matriu[i][x] == 0) {
+                    chivato = true;
                 }
             }
         }
-
-
     }
 
-    static void movimentDalt (int [][] matriu){
-
-
-
+    static void movimentDalt(int[][] matriu) {
     }
 
-    static boolean chekOutOfBounds (int [][] matriu, int row, int col){
-        boolean outOfBounds= false;
+    static boolean chekOutOfBounds(int[][] matriu, int row, int col) {
+        boolean outOfBounds = false;
 
         if (row < 0 || row > matriu.length - 1 || col < 0 || col > matriu[row].length - 1) {
 
             outOfBounds = true;
             return outOfBounds;
         }
-
         return outOfBounds;
-
-
-
-
     }
-
 }
+
+
+
+
