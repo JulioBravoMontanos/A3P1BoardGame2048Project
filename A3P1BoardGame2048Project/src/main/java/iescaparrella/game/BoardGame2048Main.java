4548893e3ba787e6d6cb6948e6032115bf155a22
JulioABravo <150833767+JulioBravoMontanos@users.noreply.github.com>
2023-12-19 15:40:37 +0100

    Update BoardGame2048Main.java

--- A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
+++ A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
@@ -1,4 +1,42 @@
-package iescaparrella.game;
+import java.util.Scanner;
 
 public class BoardGame2048Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+
+        int menu;
+
+        do {
+
+            System.out.println("Sel.leccioni una opció del menú:");
+            System.out.println("~~~~ MEN´U ~~~~\n" +
+                    "0. Sortir\n" +
+                    "1. Jugar en mode fàcil\n" +
+                    "2. Jugar en mode difíıcil\n");
+            menu= keyboard.nextInt();
+
+            switch (menu) {
+                case 0:
+
+                    System.out.println("Sortint");
+
+                    break;
+
+
+                case 1:
+                    System.out.println("Mode fàcil sel.leccionat.");
+                    break;
+
+
+                case 2:
+                    System.out.println("Mode difícil sel.leccionat.");
+                    break;
+
+            }
+
+
+
+        }while (menu!=0);
+    }
 }
