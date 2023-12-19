a2c1d612cc17d898cdc7dfd304b98403aaea5751
JuliaCarullaDAM <jcarulla2@inscaparrella.cat>
2023-12-19 15:59:10 +0100

    constants

--- A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
+++ A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
@@ -1,42 +1,36 @@
 import java.util.Scanner;
 
 public class BoardGame2048Main {
+
+    static final int TAULER = 4;
+    static final int spawn = 2;
+    static final int gunayar = 2048;
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-
-
         int menu;
 
         do {
-
             System.out.println("Sel.leccioni una opció del menú:");
-            System.out.println("~~~~ MEN´U ~~~~\n" +
+            System.out.println("~~~~ MENÚ ~~~~\n" +
                     "0. Sortir\n" +
                     "1. Jugar en mode fàcil\n" +
                     "2. Jugar en mode difíıcil\n");
-            menu= keyboard.nextInt();
+            menu = keyboard.nextInt();
 
             switch (menu) {
                 case 0:
-
                     System.out.println("Sortint");
-
                     break;
-
 
                 case 1:
                     System.out.println("Mode fàcil sel.leccionat.");
                     break;
 
-
                 case 2:
                     System.out.println("Mode difícil sel.leccionat.");
                     break;
-
             }
-
-
-
-        }while (menu!=0);
+        } while (menu != 0);
     }
 }
