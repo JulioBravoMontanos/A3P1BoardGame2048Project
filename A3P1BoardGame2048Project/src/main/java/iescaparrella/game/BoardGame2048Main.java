4f0a86a63c4086c641fb3b2760d772b70634e105
JuliaCarullaDAM <jcarulla2@inscaparrella.cat>
2023-12-19 16:50:06 +0100

    Modificiació de les funcions
    Inicialitzar la matriu del principi abams de començar el joc i mostrar la matriu

--- A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
+++ A3P1BoardGame2048Project/src/main/java/iescaparrella/game/BoardGame2048Main.java
@@ -1,3 +1,5 @@
+package iescaparrella.game;
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -11,7 +13,7 @@ public class BoardGame2048Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int menu;
-        int taula [][] = new int[TAULER][TAULER];
+        int taula[][] = new int[TAULER][TAULER];
 
         do {
             System.out.println("Sel·leccioni una opció del menú:");
@@ -28,31 +30,53 @@ public class BoardGame2048Main {
 
                 case 1:
                     System.out.println("Mode fàcil sel·leccionat.");
+                    inicialitzarTaula(taula);
+                    System.out.printf(mostrarMatriu(taula));
                     break;
 
                 case 2:
                     System.out.println("Mode difícil sel·leccionat.");
+                    inicialitzarTaula(taula);
+                    System.out.printf(mostrarMatriu(taula));
                     break;
             }
         } while (menu != 0);
     }
-    static  String mostrarMatriu(int matriu[][]){
 
+    static String mostrarMatriu(int matriu[][]) {
+        String str = "";
+        
+        for (int row = 0; row < matriu.length; row++) {
+            str += "| ";
+            for (int col = 0; col < matriu[row].length; col++) {
+                if (matriu[row][col] == 0) str += "\t| ";
+                else str += matriu[row][col] + " |";
+
+            }
+            str += "\n";
+        }
+        return str;
     }
-    static void inicialitzarTaula(int [][] matriu){
-        Random rnd = new Random;
 
-        for (int i = 0; i < matriu.length; i++){
-            for (int x = 0; x < matriu[0].length; x++){
+    static void inicialitzarTaula(int[][] matriu) {
+        Random rnd = new Random();
+        int rnd1, rnd2;
+
+        //Omplir la taula de 0
+        for (int i = 0; i < matriu.length; i++) {
+            for (int x = 0; x < matriu[0].length; x++) {
                 matriu[i][x] = 0;
             }
         }
-        matriu[rnd.newInt(4)][rnd.newInt(4)] = SPAWN;
-        //if (matriu[rnd.newInt(4)][rnd.newInt(4)] == 0 );
-        //Corregir això
-        //matriu[rnd.newInt(4)][rnd.newInt(4)] = if SPAWN;
 
+        //Afegir primer SPAWN en una cel·la random
+        matriu[rnd.nextInt(4)][rnd.nextInt(4)] = SPAWN;
 
-
+        //Control per evitar que el segon SPAWN a la mateixa cel·la que abans
+        do {
+            rnd1 = rnd.nextInt(4);
+            rnd2 = rnd.nextInt(4);
+        } while (matriu[rnd1][rnd2] != 0);
+        matriu[rnd1][rnd2] = SPAWN;
     }
 }
